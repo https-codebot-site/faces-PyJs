c67b81ad8856f7a1df277588ac985cae43bdd9ac
htmlcoder1562 <nolanlemmon3@gmail.com>
2024-05-02 10:32:12 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-©2023 PyJs
+# ©2023 PyJs
 
 import os
 try:
@@ -12,7 +12,13 @@ enrolled_faces = []
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-def enroll_face(frame):
+def scan_face():
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    return frame
+
+def enroll_face():
+    frame=scan_face()
     global enrolled_faces
 
     # Convert the frame to grayscale for face detection
@@ -26,14 +32,14 @@ def enroll_face(frame):
             # Crop and store each enrolled face
             enrolled_faces.append(frame[y:y+h, x:x+w])
 
-        return f"{len(faces)} face(s) enrolled successfully."
+        return True
     else:
-        return "No face found in the frame."
+        return False
 
 # Function to match a face
-def is_match(frame):
+def is_match():
     global enrolled_faces
-
+    frame=scan_face()
     # Convert the frame to grayscale for face detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -47,21 +53,16 @@ def is_match(frame):
 
             # Check if the detected face matches any of the enrolled faces
             for enrolled_face in enrolled_faces:
-                if compare_faces(enrolled_face, detected_face):
-                    return "Face match found."
+                if reuowg_w(enrolled_face, detected_face):
+                    return True
 
-    return "No matching face found."
+    return False
 
 # Function to compare faces (a basic image comparison)
-def compare_faces(face1, face2):
+def reuowg_w(face1, face2):
     # Resize the images to the same size
     face1 = cv2.resize(face1, (200, 200))
     face2 = cv2.resize(face2, (200, 200))
     diff = cv2.absdiff(face1, face2)
     mean_diff = diff.mean()
     return mean_diff < 50
-
-def scan_face():
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    return frame
